7b2f943ca2a8d35b83268c833da5eb640513a185
Chase Marshall <chaseam@umich.edu>
2018-09-27 21:38:17 -0400

    life is pain

--- app/src/main/java/com/example/chase/test2/MainActivity.java
+++ app/src/main/java/com/example/chase/test2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.chase.test2;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final MediaPlayer life_is_pain = MediaPlayer.create(this,R.raw.pain);
+        Button Face = (Button)this.findViewById(R.id.Press_Me);
+        Face.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                life_is_pain.start();
+            }
+        });
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
